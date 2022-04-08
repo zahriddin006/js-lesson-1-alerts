9a197bb15ca4bba16b7404fdef5153b56cd72c98
zahriddin006 <zahriddin06@gmail.com>
2022-04-08 23:52:33 +0500

    js lesson 1 homework

---
+++ js/script.js
@@ -0,0 +1,7 @@
+alert("Welcome");
+var firstName = prompt("what is your name");
+var lastName = prompt("what is your surname");
+var userAge = prompt("How old are you");
+var userJob = prompt("what is your job");
+alert("Thank you for your information");
+console.log( "Welcome" + "\n" + "Name:" + firstName + "\n" + "surname:" + lastName + "\n" + "Age:" + userAge + "\n" + "Job:" + userJob + "\n" + "Thank you for your information");
